e7009e68cc55f00ea5185bc6cb6b0799874ef8e9
Daniel Goldacker <danielgoldacker@MacBook-Air-de-Daniel.local>
2021-07-19 16:27:51 -0300

    Adicionado projeto de entrada de dados com SPLIT

---
+++ EntradaDadosSimplesSPLIT/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EntradaDadosSimplesSPLIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre o seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem sua casa?");
+            int qtdQuartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double precoProduto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe seu ultimo nome, idade e altura (Mesma linha)?");
+            string[] dados = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(nome);
+            Console.WriteLine(qtdQuartos);
+            Console.WriteLine(precoProduto);
+
+            foreach (var item in dados)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
