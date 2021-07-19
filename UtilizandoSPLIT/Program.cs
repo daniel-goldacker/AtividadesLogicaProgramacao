fa7b7eec881edb636cb97cf8bc35711c48f1f45c
Daniel Goldacker <danielgoldacker@MacBook-Air-de-Daniel.local>
2021-07-19 15:49:34 -0300

    Adicionado projeto usando SPLIT

---
+++ UtilizandoSPLIT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UtilizandoSPLIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe seu nome completo:");
+            string[] nome = Console.ReadLine().Split(' ');
+
+            Console.WriteLine("Nome quebrado por espaço: ");
+            foreach (var item in nome)
+            { 
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
