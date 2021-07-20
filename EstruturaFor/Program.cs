6416c50b21c0f83f28b1533c8f7ab12d836e544e
Daniel Goldacker <danielgoldacker@MacBook-Air-de-Daniel.local>
2021-07-20 14:31:03 -0300

    Adicionado projeto utilizando estrutura FOR

---
+++ EstruturaFor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EstruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de números inteiros que deseja somar :");
+            int qtdNumeros = int.Parse(Console.ReadLine());
+
+            double total = 0;
+            
+            for (int i = 1; i <= qtdNumeros; i++)
+            {
+                Console.WriteLine("Informe o número #{0}: ", i);
+                total += double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Total da soma é de {0}", total);
+        }
+    }
+}
