a9dfe306263e54d21158f6f9f64d088aa8ed873b
Daniel Goldacker <danielgoldacker@MacBook-Air-de-Daniel.local>
2021-07-22 17:37:09 -0300

    Adicionado projeto construtores, sobrecarga, sintaxe alternativa para iniciar valores e palavra this

--- ProdutoEstoque/Produto.cs
+++ ProdutoEstoque/Produto.cs
@@ -6,6 +6,35 @@ namespace ProdutoEstoque
         public string Nome;
         public double Preco;
         public double Estoque;
+        public string UnidadeMedida;
+
+        // Construtor padrão
+        public Produto()
+        {
+        }
+
+        // Construtor com inicialização
+        public Produto(string nome, double preco, double estoque)
+        {
+            Nome = nome;
+            Preco = preco;
+            Estoque = estoque;
+        }
+
+        // construtor com sobrecarga de método
+        public Produto(string nome, double Preco)
+        {
+            Nome = nome;
+            this.Preco = Preco;
+            /* O this nesse caso quer dizer que estou me referindo ao atributo "Preco" da classe pois nesse exemplo o nome do parâmetro 
+             * do costrutor tem o mesmo nome do atributo da classe */
+        }
+
+        // construtor com sobrecarga de método - usando o this de outro construtor da classe para preencher os campos.
+        public Produto(string nome, double preco, double estoque, string unidadeMedida): this(nome, preco, estoque)
+        {
+            UnidadeMedida = unidadeMedida;
+        }
 
         private double ValorTotalEmEstoque()
         {
--- ProdutoEstoque/Program.cs
+++ ProdutoEstoque/Program.cs
@@ -6,37 +6,46 @@ namespace ProdutoEstoque
     {
         static void Main(string[] args)
         {
-
-            Produto produto = new Produto();
             Console.WriteLine("Entre os dados do produto: ");
 
             Console.Write("Produto: ");
-            produto.Nome = Console.ReadLine();
+            string produto = Console.ReadLine();
 
             Console.Write("Preço: ");
-            produto.Preco = double.Parse(Console.ReadLine());
+            double preco = double.Parse(Console.ReadLine());
 
             Console.Write("Estoque: ");
-            produto.Estoque = int.Parse(Console.ReadLine());
+            double estoque = int.Parse(Console.ReadLine());
+
+            // Construtor com inicialização de classe
+            Produto produtoClass = new Produto(produto, preco, estoque);
+             
+            // Sitaxe alternativa para inicar valores (Não precisa de construtor)
+            Produto produtoClass3 = new Produto
+            {
+                Nome = "Rádio",
+                Preco = 129.90,
+                Estoque = 10
+            };
 
             Console.WriteLine("");
-            Console.WriteLine("Dados do Produto: {0}", produto);
+            Console.WriteLine("Dados do Produto: {0}", produtoClass);
             Console.WriteLine("");
 
             Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
             int quantidadeADD = int.Parse(Console.ReadLine());
-            produto.AdicionarProdutos(quantidadeADD);
+            produtoClass.AdicionarProdutos(quantidadeADD);
 
             Console.WriteLine("");
-            Console.WriteLine("Dados Atualizados: {0}", produto);
+            Console.WriteLine("Dados Atualizados: {0}", produtoClass);
             Console.WriteLine("");
 
             Console.WriteLine("Digite o número de produtos a ser removida do estoque: ");
             int quantidadeREM = int.Parse(Console.ReadLine());
-            produto.RemoverProdutos(quantidadeREM);
+            produtoClass.RemoverProdutos(quantidadeREM);
 
             Console.WriteLine("");
-            Console.WriteLine("Dados Atualizados: {0}", produto);
+            Console.WriteLine("Dados Atualizados: {0}", produtoClass);
             Console.WriteLine("");
         }
     }
