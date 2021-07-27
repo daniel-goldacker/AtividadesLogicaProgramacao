819d06058bd0ed2ec6cb81b1442a62ba1450c36f
Daniel Goldacker <danielgoldacker@MacBook-Air-de-Daniel.local>
2021-07-27 13:53:49 -0300

    Ajustado projeto de struct adicionando comentário e exemplos

--- UtilizandoStruct/Program.cs
+++ UtilizandoStruct/Program.cs
@@ -6,19 +6,27 @@ namespace UtilizandoStruct
     {
         static void Main(string[] args)
         {
-            // utilizando struct não precisa intanciar;
-            Point p;
+            // utilizando struct não precisa instanciar;
+            Point p1;
 
-            p.X = 10;
-            p.Y = 20;
+            p1.X = 10;
+            p1.Y = 20;
 
-            Console.WriteLine(p);
+            Console.WriteLine(p1);
 
-            // Paga gerar um novo objeto basta dar um new
-            p = new Point();
-            Console.WriteLine(p);
+            /* Ao fazer isso no struct ele atribui o valor de p1 em uma nova variável chamada p2 não fazendo referência a um ponteiro de 
+             * memória como em uma classe */
+            Point p2 = p1; 
+            p2.X = 99;
+
+            Console.WriteLine(p2);
+            Console.WriteLine(p1);
 
 
+            // Para gerar um novo objeto(struct) basta dar um new assim como em uma classe.
+            p1 = new Point();
+            Console.WriteLine(p1);
+           
         }
     }
 }
