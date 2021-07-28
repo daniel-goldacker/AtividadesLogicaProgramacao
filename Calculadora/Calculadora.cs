a9cf260058d14cf98b8bde1b8024e4a6bc6571b4
Daniel Goldacker <daniel-goldacker@hotmail.com>
2021-07-28 19:28:38 -0300

    Adicionado projeto calculadora

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Calculadora
+{
+    public class Calculadora
+    {
+
+        public static double Soma(params double[] numeros)
+        {
+            double soma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                soma += numeros[i];
+            }
+
+            return soma;
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Calculadora.Soma(new double[]{1,2,3}));
+
+            Console.WriteLine(Calculadora.Soma(3, 2, 3 )); /* colocando o params na frente do vetor no método da classe não necessita 
+                                                            * intanciar um vetor e passar como parâmetro, conforme exemplo acima */
+        }
+    }
+}
