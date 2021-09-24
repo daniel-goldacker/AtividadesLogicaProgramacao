3196427a26350351ddca375d3c951f87860524b4
Daniel Goldacker <daniel-goldacker@hotmail.com>
2021-09-24 09:41:55 -0300

    Utilizando Try/Catch

---
+++ UtilizandoTryCath/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UtilizandoTryCath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Informe o valor 1: ");
+                int valor1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe o valor 2: ");
+                int valor2 = int.Parse(Console.ReadLine());
+
+                int resultado = valor1 / valor2;
+                Console.WriteLine("");
+                Console.Write("Valor: {0}", resultado);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("ERRO: Não é permitido divisão por zero");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("ERRO: Erro na formatação ({0})", e.Message);
+            }
+        }
+    }
+}
